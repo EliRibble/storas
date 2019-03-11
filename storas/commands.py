2f3524f9cee8a897172820fa83a1032756c5c638
Eli Ribble <eliribble@google.com>
2019-03-11 14:12:49 -0700

    Add initial support for actually cloning the manifests.
    This just gets the manifests.git directory so we can start reading manifests.

--- storas/commands.py
+++ storas/commands.py
@@ -1,7 +1,24 @@
 """Logic for all subcommands."""
 import argparse
+import logging
+import os
+import typing
+import subprocess
+
+LOGGER = logging.getLogger(__name__)
+
+def _run_git(args: typing.List[typing.Text], cwd: typing.Text) -> None:
+	args = ["git", "-C", cwd] + args
+	LOGGER.debug("Running '%s'", " ".join(args))
+	process = subprocess.run(args, check=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+	LOGGER.info("Result: %s", process.stdout)
 
 def init(args: argparse.Namespace) -> None:
 	"""Initialize a build area."""
-	del args
-	print("hey")
+	branch = args.branch
+	url = args.url
+
+	repo_path = os.path.join(os.path.abspath("."), ".repo")
+	LOGGER.info("Creating repo directory at %s", repo_path)
+	os.makedirs(repo_path, exist_ok=True)
+	_run_git(["clone", "-b", branch, url, "manifests.git"], repo_path)
--- storas/main.py
+++ storas/main.py
@@ -1,5 +1,7 @@
 """Main entrypoint module. Used by bin/storas."""
 import argparse
+import logging
+import subprocess
 
 import storas.commands
 
@@ -10,14 +12,22 @@ def run() -> int:
 		0 on success, nonzero on failure.
 	"""
 	parser = argparse.ArgumentParser()
+	parser.add_argument("--verbose", action="store_true", help="Show more verbose logging")
 
 	subparsers = parser.add_subparsers(title="command", help="The command to perform.")
 	init = subparsers.add_parser("init")
 	init.set_defaults(command=storas.commands.init)
-	init.add_argument("-u", required=True, help="The URL to pull the manifest file from")
-	init.add_argument("-b", default="master", help="The branch to use for the manifest.")
+	init.add_argument("-b", "--branch", default="master", help="The branch to use for the manifest.")
+	init.add_argument("-u", "--url", required=True, help="The URL to pull the manifest file from")
 
 	args = parser.parse_args()
 
-	args.command(args)
+	logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+	try:
+		args.command(args)
+	except subprocess.CalledProcessError as err:
+		logging.error("Subprocess call '%s' failed (%d)", err.args, err.returncode)
+		logging.error("stdout: %s", err.stdout)
+		logging.error("stderr: %s", err.stderr)
+		return 1
 	return 0
