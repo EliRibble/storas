4ad058baed1b26d6dd57cb3a108726738042052a
Eli Ribble <eliribble@google.com>
2020-08-24 06:28:30 -0700

    Fix setup.py whitespace, add twine for uploading package.

--- setup.py
+++ setup.py
@@ -2,26 +2,27 @@
 from setuptools import setup # type: ignore
 
 setup(
-    name="storas",
-    version="0.1",
-    description="A replacement for Android's repo tool",
-    url=None,
-    author="Eli Ribble",
-    extras_require={
-        "develop" : [
-            "mypy==0.770",
-            "nose2",
-            "pre-commit==1.14.4",
-            "pylint==2.3.1",
-            "wheel",
-        ]
-    },
-    install_requires=[
-    ],
-    scripts=[
-    ],
-    packages=['storas'],
-    package_data={
-       'storas' : ['storas/*'],
-    },
+	name="storas",
+	version="0.1",
+	description="A replacement for Android's repo tool",
+	url=None,
+	author="Eli Ribble",
+	extras_require={
+		"develop" : [
+			"mypy",
+			"nose2",
+			"pre-commit",
+			"pylint",
+			"twine",
+			"wheel",
+		]
+	},
+	install_requires=[
+	],
+	scripts=[
+	],
+	packages=['storas'],
+	package_data={
+	   'storas' : ['storas/*'],
+	},
 )
